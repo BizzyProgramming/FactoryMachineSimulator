1c07e63f00c4c215d250f1c38e1dbd24270c6ae9
Bruce Brown <brucebrown2008@hotmail.com>
2026-08-16 20:51:59 -0400

    Add Home class with main method for application

---
+++ src/bizzy_2025/Home.java
@@ -0,0 +1,12 @@
+package bbrown2025;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MachineOperator operator = new MachineOperator();
+        System.out.println("Welcome to Bizzys AutoMachine\n");
+        operator.operateMachine();
+	}
+
+}
